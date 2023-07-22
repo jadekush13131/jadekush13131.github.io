981c26b1a2709a495aea211f39361df8060724e3
jadekush13131 <jade3344232.2@gmail.com>
2023-07-22 17:31:51 +0530

    add tunnel.sh

---
+++ tunnel.sh
@@ -0,0 +1,11 @@
+if [[ $(command -v sudo) ]];then
+a="SUDO"
+else
+a="NO_SUDO"
+fi
+
+if [[ $a == "SUDO" ]];then
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflare-linux-amd64.deb && rm cloudflared-linux-amd64.deb
+else
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && dpkg -i cloudflare-linux-amd64.deb rm cloudflared-linux-amd64.deb
+fi
