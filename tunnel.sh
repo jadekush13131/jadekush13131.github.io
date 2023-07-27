afbff9707b2cec7a01129b284d9b76cfd5e0e947
jadekush13131 <jade3344232.2@gmail.com>
2023-07-27 15:49:08 +0530

    update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -7,5 +7,5 @@ fi
 if [[ $a == "SUDO" ]];then
 wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared-linux-amd64.deb && rm cloudflared-linux-amd64.deb
 else
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && dpkg -i cloudflared-linux-amd64.deb rm cloudflared-linux-amd64.deb
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && dpkg -i cloudflared-linux-amd64.deb && rm cloudflared-linux-amd64.deb
 fi
