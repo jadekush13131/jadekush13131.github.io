c846d6293c8bea67d2a6cdaa70f3d45958eda616
jadekush13131 <104832023+jadekush13131@users.noreply.github.com>
2023-10-18 17:56:21 +0530

    Update jdown.sh

--- jdown.sh
+++ jdown.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
 wget -q http://installer.jdownloader.org/JDownloader.jar
-java -jar JDownloader.jar -norestart
+java -jar JDownloader.jar -norestart > /dev/null 2>&1
+echo "Installed and Update JDownloader and restarting server ...."
+
+read -p "Enter email: " email
+read -p "Enter password: " password
+
+# JSON string
+json='{"email":"", "password":"", "devicename":"linux@root", "directconnectmode":"LAN"}'
+
+# Use awk to update the JSON
+updated_json=$(echo "$json" | awk -v email="$email" -v password="$password" '{
+  gsub(/\"email\":\"[^\"]*\"/, "\"email\":\"" email "\"");
+  gsub(/\"password\":\"[^\"]*\"/, "\"password\":\"" password "\"");
+  print
+}')
+
+# Save the updated JSON to the settings.json file
+echo "$updated_json" > cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
+
 java -jar JDownloader.jar -norestart
