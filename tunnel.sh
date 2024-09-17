0e7199d610f681349bcb156d450e2ff3b95c421d
jadekush13131 <104832023+jadekush13131@users.noreply.github.com>
2024-09-17 18:32:16 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -5,7 +5,7 @@ a="NO_SUDO"
 fi
 
 if [[ $a == "SUDO" ]];then
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared-linux-amd64.deb && rm cloudflared-linux-amd64.deb
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && sudo chmod +x cloudflared-linux-amd64 && sudo mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
 else
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && dpkg -i cloudflared-linux-amd64.deb && rm cloudflared-linux-amd64.deb
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && chmod +x cloudflared-linux-amd64 && mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
 fi
