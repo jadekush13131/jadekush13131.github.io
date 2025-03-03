e9c6a3839ae4aa740dc344f22df7a1f8eb17545f
jadekush13131 <104832023+jadekush13131@users.noreply.github.com>
2025-03-03 18:44:09 +0530

    Add files via upload

---
+++ gofile.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if ! command -v aria2c > /dev/null 2>&1; then
+    echo "aria2c is not installed" >&2
+    exit 1
+fi
+
+if ! command -v wget > /dev/null 2>&1 || ! command -v curl > /dev/null 2>&1; then
+    echo "wget and curl is installed, install both of them first."
+    exit 1
+fi
+
+if ! command -v jq > /dev/null 2>&1; then
+     echo "jq is not installed, Installing ...."
+     wget "https://github.com/jqlang/jq/releases/latest/download/jq-linux-amd64" > /dev/null 2>&1
+     chmod +x jq-linux-amd64
+     mv jq-linux-amd64 /usr/bin/jq
+     echo "Done."
+     exit
+fi
+
+if [ -z "$1" ] || ! echo "$1" | grep -q '^https://gofile.io/d/'; then
+    echo "Need a valid URL" >&2
+    echo "aria2c should be installed to work" >&2
+    exit 1
+fi
+
+token=$(curl -s -X POST https://api.gofile.io/accounts | jq -r .data.token )
+
+link=$1
+Id=$(echo "$link" | cut -d'/' -f5)
+
+webToken="4fd6sg89d7s6"
+
+content="https://api.gofile.io/contents/$Id?wt=$webToken"
+
+valid=$(curl -s -H "Authorization: Bearer $token" "$content" | jq -r .status)
+
+if [ "$valid" != "ok" ]; then
+    echo "File does not exist" >&2
+    exit 1
+fi
+
+dl_link=$(curl -s -H "Authorization: Bearer $token" "$content" | jq -r .data.children[].link)
+echo "$dl_link" > /tmp/temp.txt
+
+aria2c -x3 --header="Authorization: Bearer $token" -i /tmp/temp.txt -Z
+
+rm /tmp/temp.txt
