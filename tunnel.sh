98e749dfcf78c7f2e0c0f9ea6a069bbe9aebdca0
jadekush13131 <104832023+jadekush13131@users.noreply.github.com>
2025-04-07 17:23:41 +0530

    added wget error handling and optimize sudo usage

--- tunnel.sh
+++ tunnel.sh
@@ -1,11 +1,22 @@
-if [[ $(command -v sudo) ]];then
-a="SUDO"
-else
-a="NO_SUDO"
+#!/bin/sh
+
+if ! command -v wget > /dev/null 2>&1; then
+    echo "wget is not installed."
+    exit 1
 fi
 
-if [[ $a == "SUDO" ]];then
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && sudo chmod +x cloudflared-linux-amd64 && sudo mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
+if command -v sudo > /dev/null 2>&1; then
+    a="SUDO"
 else
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && chmod +x cloudflared-linux-amd64 && mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
+    a="NO_SUDO"
+fi
+
+if [ "$a" = "SUDO" ]; then
+    wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+    sudo chmod +x cloudflared-linux-amd64
+    sudo mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
+else
+    wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+    chmod +x cloudflared-linux-amd64
+    mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
 fi
