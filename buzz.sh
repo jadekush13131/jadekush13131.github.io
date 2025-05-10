f07ee12d5871be49671c62f9b0458ca430119c81
jadekush13131 <104832023+jadekush13131@users.noreply.github.com>
2025-05-10 19:33:12 +0530

    Update buzz.sh

--- buzz.sh
+++ buzz.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z "$1" ] || ! echo "$1" | grep -q '^https://buzzheavier.com'; then
-    echo "need url" >&2
-    exit 1
-fi
 if ! command -v curl > /dev/null 2>&1 || ! command -v aria2c > /dev/null 2>&1;then
         echo "Either curl or aria2c is not installed"
         exit 1
+fi
+if [ -z "$1" ] || ! echo "$1" | grep -q '^https://buzzheavier.com'; then
+    echo "need url" >&2
+    exit 1
 fi
 link=$1
 dl_link=$(curl -sD - "$1/download"  --header "Referer: $1" | grep -i "hx-redirect" | sed 's/.*hx-redirect: //I')
