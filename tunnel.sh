eb59c61162ae0cbf5a7bfb7bb5a19158497b5463
jadekush13131 <104832023+jadekush13131@users.noreply.github.com>
2025-05-28 13:50:28 +0530

    Added run as root

--- tunnel.sh
+++ tunnel.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
-
+green="\033[0;32m"
+rc="\033[0m"
+red="\033[0;31m"
+if [ "$(id -u)" -ne 0 ]; then
+  printf "${red}This script must be run as root. Exiting. ${rc}\n"
+  exit 1
+fi
 if ! command -v wget > /dev/null 2>&1; then
     echo "wget is not installed."
     exit 1
 fi
+wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+chmod +x cloudflared-linux-amd64
+mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
 
-if command -v sudo > /dev/null 2>&1; then
-    a="SUDO"
-else
-    a="NO_SUDO"
-fi
-
-if [ "$a" = "SUDO" ]; then
-    wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-    sudo chmod +x cloudflared-linux-amd64
-    sudo mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
-else
-    wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-    chmod +x cloudflared-linux-amd64
-    mv cloudflared-linux-amd64 /usr/local/bin/cloudflared
-fi
