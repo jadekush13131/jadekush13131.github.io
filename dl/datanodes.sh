d57905eefb13d8bec160ce832a3737ba4113f637
jadekush13131 <jade3344232.2@gmail.com>
2026-01-07 11:24:01 +0000

    update datanodes dl script

--- dl/datanodes.sh
+++ dl/datanodes.sh
@@ -4,12 +4,25 @@ set -e
 
 [ "$#" -eq 0 ] && { echo "No arguments given" >&2; exit 1; }
 
+if ! echo "$1" | grep -qE '^https://datanodes.to/' ;then
+  printf "need valid url.\n"
+  exit 1
+fi
+
 if ! command -v aria2c > /dev/null 2>&1 || ! command -v curl > /dev/null 2>&1 || ! command -v jq > /dev/null 2>&1; then
   echo "make sure aria2c, curl and jq are installed. exiting.."
   exit 1
 fi
 
+id=$(echo "$1" | cut -d/ -f4)
 
-dl=$(curl -s -X POST "https://datanodes.to/download" -F "op=download2"  -F "id=$1"  -F "rand="  -F "referer=https://datanodes.to/download"  -F "method_free=Free Download >>"  -F "method_premium="  -F "__dl=1" | jq -r .url | sed 's/%/\\x/g' | xargs -0 printf "%b")
+url=$(curl -s -X POST "https://datanodes.to/download" -F "op=download2"  -F "id=$id"  -F "rand="  -F "referer=https://datanodes.to/download"  -F "method_free=Free Download >>"  -F "method_premium="  -F "__dl=1") 
+
+if ! echo "$url" | grep -qE '^\{'; then
+  printf "errored. something went wrong.\n"
+  exit 1
+fi
+
+dl=$(echo "$url" | jq -r .url  | sed 's/%/\\x/g' | xargs -0 printf "%b")
 
 aria2c -x3 -c "$dl"
